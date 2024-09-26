f280104e9e17aec1b5873bdaef3f00a80d2a04bf
gdragonedev <gdragonedev@gmail.com>
2024-09-25 21:22:08 -0400

    Added drawing lines to Window, created Cells.

--- src/graphics.py
+++ src/graphics.py
@@ -2,17 +2,16 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-
         #create root widget
         self.__root = Tk()
         self.__root.title("My Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         #create and pack canvas for drawing
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas = Canvas(
+            self.__root, bg="white", height=height, width=width
+        )
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-
-
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -20,9 +19,29 @@ class Window:
 
     def wait_for_close(self):
         self.__running = True
-        while self.running:
+        while self.__running:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self):
-        self.running = False
+        self.__running = False
+    
+
+class Point:
+    def __init__(self, x, y):
+        #coords of point in px
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
