8d2c23472cff9d59a6d492b3c256c5a8b9e01673
gdragonedev <gdragonedev@gmail.com>
2024-10-03 20:16:25 -0400

    Added tests. Created entrance and exit to maze.

--- src/maze.py
+++ src/maze.py
@@ -10,7 +10,7 @@ class Maze:
             num_cols,
             cell_size_x,
             cell_size_y,
-            win
+            win=None
         ):
         self._cells = []
         self._x1 = x1
@@ -22,6 +22,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
     
     def _create_cells(self):
         #establish maze cells
@@ -35,6 +36,8 @@ class Maze:
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
+
+
 
     def _draw_cell(self, i, j):
         if self._win is None:
@@ -51,3 +54,9 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.005)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
