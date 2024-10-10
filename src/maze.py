e496b4a373aea6ac0ecc84e2fc28ed941705dfb4
gdragonedev <gdragonedev@gmail.com>
2024-10-09 20:17:08 -0400

    Added solver method, and tweaked console msgs.

--- src/maze.py
+++ src/maze.py
@@ -89,12 +89,14 @@ class Maze:
                 self._draw_cell(i, j)
                 return
             
+            #randomize next direction to move
             target_index = random.randrange(len(to_visit_list))
             to_visit = to_visit_list[target_index]
             target_i = to_visit[0]
             target_j = to_visit[1]
             direction = to_visit[2]
             
+            #match and break current, and next cells shared walls
             match direction:
                 case "left":
                     self._cells[target_i][target_j].has_right_wall = False
@@ -109,9 +111,60 @@ class Maze:
                     self._cells[target_i][target_j].has_top_wall = False
                     self._cells[i][j].has_bottom_wall = False
 
+            #recurse to next(target) cell
             self._break_walls_r(target_i, target_j)
 
     def _reset_cells_visited(self):
         for col in self._cells:
             for cell in col:
                 cell.visited = False
+
+    #returns True if this is the end cell, OR if the current path hasn't been proven to be incorrect
+    #returns False if current cell cannot be part of solution path
+    def _solve_r(self, i, j):
+        self._animate()
+
+        #mark current as visited
+        self._cells[i][j].visited = True
+
+        #if current is end, the maze is solved
+        if i == self._num_cols - 1 and j == self._num_rows - 1:
+            return True
+        
+        #try left
+        if i > 0 and not self._cells[i-1][j].visited and not self._cells[i][j].has_left_wall:
+            self._cells[i][j].draw_move(self._cells[i-1][j])
+            if self._solve_r(i-1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i-1][j], True)
+        
+        #check adjacent cell in all directions, visit if they haven't been visited, and no wall exists between current, and neighbor
+        #try right
+        if i < self._num_cols - 1 and not self._cells[i+1][j].visited and not self._cells[i][j].has_right_wall:
+            self._cells[i][j].draw_move(self._cells[i+1][j])
+            if self._solve_r(i+1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i+1][j], True)
+        #try top
+        if j > 0 and not self._cells[i][j-1].visited and not self._cells[i][j].has_top_wall:
+            self._cells[i][j].draw_move(self._cells[i][j-1])
+            if self._solve_r(i, j-1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j-1], True)
+        #try bottom
+        if j < self._num_rows - 1 and not self._cells[i][j+1].visited and not self._cells[i][j].has_bottom_wall:
+            self._cells[i][j].draw_move(self._cells[i][j+1])
+            if self._solve_r(i, j+1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j+1], True)
+
+        #wrong path traversed, go back to offending cell, and continue    
+        return False
+    
+    #discover solution using depth first search
+    def solve(self):
+        return self._solve_r(0, 0)
